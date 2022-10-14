2f2f7f4e26f7ad139b63ac8cb70ea61d499227b2
mohammedalgamal <mohammedalgamal95@gmail.com>
2022-10-14 21:27:56 +0200

    Make the function getUserChoice

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const choices = ['rock', 'paper', 'scissors'];
+const CHOICES = ['rock', 'paper', 'scissors'];
 
 function random(start, end) {
     return (Math.floor(Math.random() * (end - start)) + start);
 }
 
 function getComputerChoice() {
-    return choices[random(0, choices.length)]
+    return CHOICES[random(0, CHOICES.length)]
+}
+
+function getUserChoice() {
+    do {
+        playerSelection = prompt("What is your choice?");
+        if (playerSelection === null) {
+            alert('please type in a choice!');
+            continue;
+        }
+        playerSelection = playerSelection.toLowerCase();
+        if (!CHOICES.includes(playerSelection)) {
+            alert('invalid choice');
+            continue
+        }
+    } while (!CHOICES.includes(playerSelection) || playerSelection === null);
+    
+    return playerSelection;
 }
