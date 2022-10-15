0615adf0e48fc864ec689d42ae23f7ab69e6b524
mohammedalgamal <mohammedalgamal95@gmail.com>
2022-10-15 09:43:31 +0200

    Add function whoWins

--- script.js
+++ script.js
@@ -24,3 +24,23 @@ function getUserChoice() {
     
     return playerSelection;
 }
+
+function whoWins(choice1, choice2) {
+    /* This function returns 0 if it's a tie
+    , a positive value if player1 wins
+    , and a negative value if player2 wins */
+
+    // Define needed variables
+    let val1 = CHOICES.indexOf(choice1);
+    let val2 = CHOICES.indexOf(choice2);
+    let diff = val1 - val2;
+
+    // Reverse diff sign if the difference is 2
+    if (Math.abs(diff) === 2) {
+        diff = -diff;
+    }
+
+    return diff;
+}
+
+
