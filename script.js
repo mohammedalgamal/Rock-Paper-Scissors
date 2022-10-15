9a06ad871a43f1e8bc25c58aaf802d4e75887333
mohammedalgamal <mohammedalgamal95@gmail.com>
2022-10-15 11:23:51 +0200

    Add the function game
    This commit adds the function game which calls playRound
    5 times, and makes a full game of 5 rounds then logs the winner.
    In order to make this function some modifications to other functions
    were made and the following are theses modifications:
    
        - Changed the function whoWins to return 1 and -1 (normalized),
        to use these values to determine scores in game.
    
        - Changed the return values of function playRound to return
        a 2 element array, where the first element is the string for
        the result and second is the value of the round.
    
        - Made a call to the function game to start the program.

--- script.js
+++ script.js
@@ -27,17 +27,17 @@ function getPlayerChoice() {
 
 function whoWins(choice1, choice2) {
     /* This function returns 0 if it's a tie
-    , a positive value if player1 wins
-    , and a negative value if player2 wins */
+    , 1 if player1 wins
+    , -1 if player2 wins */
 
     // Define needed variables
     let val1 = CHOICES.indexOf(choice1);
     let val2 = CHOICES.indexOf(choice2);
     let diff = val1 - val2;
 
-    // Reverse diff sign if the difference is 2
+    // Reverse diff sign if the difference is 2 and normalize it
     if (Math.abs(diff) === 2) {
-        diff = -diff;
+        diff = (-diff / diff);
     }
 
     return diff;
@@ -49,12 +49,65 @@ function playRound(playerSelection, computerSelection) {
     let gameValue = whoWins(playerSelection, computerSelection);
 
     if (gameValue === 0) {
-        return `It's a tie, you both chose ${playerSelection}.`;
+        return [`It's a tie, you both chose ${playerSelection}.`, gameValue];
     }
     else if (gameValue > 0) {
-        return `You win! ${playerSelection} beats ${computerSelection}.`;
+        return [`You win! ${playerSelection} beats ${computerSelection}.`, gameValue];
     }
     else {
-        return `Computer wins! ${computerSelection} beats ${playerSelection}.`;
+        return [`Computer wins! ${computerSelection} beats ${playerSelection}.`, gameValue];
     }
 }
+
+function game() {
+    
+    // Variables to store scores
+    let playerScore = 0;
+    let computerScore = 0;
+
+    // Play 5 rounds
+    for (let i = 0; i < 5; i++) {
+        // Call function playRound and score its values
+        let round = playRound(getPlayerChoice(), getComputerChoice());
+        
+        // Update scores
+        if (round[1] > 0) {
+            playerScore += round[1];
+        }
+        else if (round[1] < 0) {
+            computerScore += -round[1];
+        }
+        
+        // Log the round result and scores
+        console.log('-----------------------------------------');
+        console.log(round[0]);
+        console.log(`Your current score: ${playerScore}`);
+        console.log(`Computer current score: ${computerScore}`);
+
+        // Return if one of the computer or player scored 3
+        if (playerScore >= 3) {
+            console.log('You win this game!');
+            return;
+        }
+        else if (computerScore >= 3) {
+            console.log('You Lose! computer scored 3.');
+            return;
+        }
+    }
+
+    // Log scores after 5 rounds
+    if (playerScore === computerScore) {
+        console.log(`The game is tied, you both scored ${computerScore}.`);
+    }
+    else if (playerScore > computerScore) {
+        console.log(`You win! Your final score is ${playerScore}, and computer final score is ${computerScore}.`);    
+    }
+    else {
+        console.log(`You lose! Your final score is ${playerScore}, and computer final score is ${computerScore}.`);    
+    }
+
+    // Terminate the function
+    return;
+}
+
+game()
